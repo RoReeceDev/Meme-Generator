1f36d0d3cfe19a8ec41c6c6e810d7b9ce1c6aeda
RoReeceDev <roreecedev@gmail.com>
2024-01-25 15:56:36 -0500

    Creating react app and header set up

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+
+    return (
+        <div className='card'>
+          <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header
